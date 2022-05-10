b340ec9a4ee7dc239ca0ad8e7796c6a2e5963560
Ahmet AYDIN <ahmet_ayd@msn.com>
2022-05-10 17:06:26 +0300

    0.1-alpha test updates

--- controller/controller.sh
+++ controller/controller.sh
@@ -1,1 +1,7 @@
 #!/bin/bash
+ansible-playbook -c local playbooks/img.yml
+ansible-playbook -c local playbooks/ins.yml
+ansible-playbook -c local playbooks/engine.yml
+ansible-playbook -c local playbooks/int.yml
+ansible-playbook -c local playbooks/gitops.yml
+#ansible-playbook -c local playbooks/app.yml
