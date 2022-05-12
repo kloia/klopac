e14af22b165a576f5b339a3395e627adad7ba4a1
Muhammed Said Kaya <muhammed.kaya@kloia.com>
2022-05-12 14:02:41 +0300

    fix output path

--- entrypoint/pkg/flow/Flow.go
+++ entrypoint/pkg/flow/Flow.go
@@ -72,7 +72,7 @@ func (p flowService) Run(provision, validate, healthCheck bool, logLevel string,
 		ansible-playbook finalizer.yml -e COMPRESS_BUNDLE=%v;
 	`, logLevel, healthCheck, !file))
 		log.Info("[FINALIZER - OUTPUT]")
-		dat, err := os.ReadFile("data/bundle/output.md")
+		dat, err := os.ReadFile("/data/bundle/output.md")
 		if err != nil {
 			log.Panic("Could not read file")
 		}
