f2d2982b11fe6ee47e53d283effae93b50b19b91
Alp Kahvecioglu <alp@kloia.com>
2022-05-18 22:47:13 +0300

    feat: read manifest files for different layers

--- provisioner-python/provisioner/__main__.py
+++ provisioner-python/provisioner/__main__.py
@@ -1,6 +1,47 @@
 from provisioner.core import *
 
+data_path = ".."
+vars_path = f"{data_path}/vars"
+manifests_path = f"{data_path}/manifests"
+
 if __name__ == "__main__":
-    ins_yaml = read_yaml_file("instance.yml")
-    engine_yaml = read_yaml_file("engine.yml")
-    print(engine_yaml)
+    instance_yaml = read_yaml_file(f"{vars_path}/instance.yaml")
+    engine_yaml = read_yaml_file(f"{vars_path}/engine.yaml")
+    image_yaml = read_yaml_file(f"{vars_path}/image.yaml")
+    platform_yaml = read_yaml_file(f"{vars_path}/platform.yaml")
+
+    instance_defaults_yaml = read_yaml_file(f"{vars_path}/defaults/ins-{instance_yaml['ins']['type']}.yaml")
+    image_defaults_yaml = read_yaml_file(f"{vars_path}/defaults/img-{image_yaml['img']['type']}.yaml")
+    engine_defaults_yaml = read_yaml_file(f"{vars_path}/defaults/engine-{engine_yaml['engine']['type']}.yaml")
+
+    dict_merge(instance_yaml, instance_defaults_yaml)
+    dict_merge(image_yaml, image_defaults_yaml)
+    dict_merge(engine_yaml, engine_defaults_yaml)
+
+    engine = engine_yaml['engine']
+    image = image_yaml['img']
+    instance = instance_yaml['ins']
+
+    if check_key(engine[engine['type']], key='branch'):
+        engine_manifest_path = f"{manifests_path}/{engine['runner']['type']}/{engine['type']}@{engine[engine['type']]['branch']}.yaml"
+        dict_merge(engine_yaml, read_yaml_file(engine_manifest_path))
+
+    if not check_key(engine[engine['type']], key='branch') and check_key(engine[engine['type']], key='version'):
+        engine_manifest_path = f"{manifests_path}/{engine['runner']['type']}/{engine['type']}-{engine[engine['type']]['version']}.yaml"
+        dict_merge(engine_yaml, read_yaml_file(engine_manifest_path))
+
+    if check_key(image[image['type']], key='branch'):
+        image_manifest_path = f"{manifests_path}/{image['runner']['type']}/{image['type']}@{image[image['type']]['branch']}.yaml"
+        dict_merge(image_yaml, read_yaml_file(image_manifest_path))
+
+    if not check_key(image[image['type']], key='branch') and check_key(image[image['type']], key='version'):
+        image_manifest_path = f"{manifests_path}/{image['runner']['type']}/{image['type']}-{image[image['type']]['version']}.yaml"
+        dict_merge(image_yaml, read_yaml_file(image_manifest_path))
+
+    if check_key(instance[instance['type']], key='branch'):
+        instance_manifest_path = f"{manifests_path}/{instance['runner']['type']}/{instance['type']}@{instance[instance['type']]['branch']}.yaml"
+        dict_merge(instance_yaml, read_yaml_file(instance_manifest_path))
+
+    if not check_key(instance[instance['type']], key='branch') and check_key(instance[instance['type']], key='version'):
+        instance_manifest_path = f"{manifests_path}/{instance['runner']['type']}/{instance['type']}-{instance[instance['type']]['version']}.yaml"
+        dict_merge(instance_yaml, read_yaml_file(instance_manifest_path))
