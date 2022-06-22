4cb84f7bcb9ea0ab181ac165444c5b51070b707a
Alp Kahvecioglu <alp@kloia.com>
2022-06-22 19:13:01 +0300

    feat: add azure subscription and tenant checks

--- validator-python/validator/platform.py
+++ validator-python/validator/platform.py
@@ -5,7 +5,10 @@ class Platform:
     def __init__(self, platform: dict) -> None:
         self._raw = platform
         self._data = platform["platform"]
-        self.auth_type = self.data["provider"]["auth"]["type"]
+        self.auth = self.data["provider"]["auth"]
+        self.auth_type = self.auth["type"]
+        self.azure_subscription = self.auth["subscription"]
+        self.azure_tenant = self.auth["tenant"]
         self.provider_name = self.data["provider"]["name"]
         self.provider_type = self.data["provider"]["type"]
 
@@ -21,6 +24,7 @@ class Platform:
         self.check_auth()
         self.check_provider_name()
         self.check_provider_type()
+        self.check_auth_azure()
 
     def check_auth(self):
         logging.info("[*] Checking auth type for the specified provider")
@@ -55,3 +59,25 @@ class Platform:
             raise Exception('[!] Provider type should be "vm" or "vmss" for Azure')
 
         raise NotImplementedError("[!] Unsupported provider name")
+
+    def check_auth_azure(self):
+        if self.provider_name == "azure":
+            logging.info("[*] Checking required auth fields for Azure")
+            self.check_azure_subscription()
+            self.check_azure_tenant()
+
+    def check_azure_subscription(self):
+        logging.info("[*] Checking subscription field for Azure")
+        if len(self.azure_subscription) > 0:
+            logging.info("[*] PASS")
+            return
+
+        raise Exception("[!] Subscription field is required for Azure")
+
+    def check_azure_tenant(self):
+        logging.info("[*] Checking tenant field for Azure")
+        if len(self.azure_tenant) > 0:
+            logging.info("[*] PASS")
+            return
+
+        raise Exception("[!] Tenant field is required for Azure")
